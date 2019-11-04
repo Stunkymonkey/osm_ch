49dd903238162973032927c84cb6895568985410
Felix Buehler <account@buehler.rocks>
2019-11-04 13:17:31 +0100

    [web] update request to have a valid request

--- web/request.sh
+++ web/request.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -d '{"start":{"latitude": 9.9937,"longitude": 53.5511}, "end":{"latitude": 9.1829,"longitude": 48.7758}}' -H "Content-Type: application/json" -X POST http://localhost:8080/dijkstra
+curl -d '{"start":{"latitude": 53.5511,"longitude": 9.9937}, "end":{"latitude": 48.7758,"longitude": 9.1829},"use_car":true,"by_distance":false}' -H "Content-Type: application/json" -X POST http://localhost:8080/dijkstra
 echo ""
