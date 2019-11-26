c329a0f9485e86db0e473cb4d365ca006874de62
Simon Matejetz <simon.mat@t-online.de>
2019-11-26 13:26:19 +0100

    add thin and dashed lines between off track start and end pointers and the real start and end nodes

--- web/static/leaflet_script.js
+++ web/static/leaflet_script.js
@@ -1,15 +1,15 @@
 var map = L.map('map', {
-	maxBounds: [
-		[47.3, 5.9], // Southwest coordinates
-		[54.9, 16.9512215] // Northeast coordinates
-	],
+    maxBounds: [
+        [47.3, 5.9], // Southwest coordinates
+        [54.9, 16.9512215] // Northeast coordinates
+    ],
 }).setView([51.1657, 10.4515], 6);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	minZoom: 6,
-	id: 'mapbox.streets',
-	accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 6,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 }).addTo(map);
 
 map.on('click', onMapClick);
@@ -25,169 +25,184 @@ var last_path;
 let xhr = new XMLHttpRequest();
 
 function onMapClick(e) {
-	if (tmpMarker) {
-		map.removeLayer(tmpMarker);
-	}
-	tmpMarker = L.marker(e.latlng).addTo(map);
-	tmpMarker.setLatLng(e.latlng);
-	tmpMarker.bindPopup("<button class='set-point set-start' onclick='setStart()''>Set Start</button><button class='set-point set-end' onclick='setEnd()''>Set End</button>").openPopup();
+    if (tmpMarker) {
+        map.removeLayer(tmpMarker);
+    }
+    tmpMarker = L.marker(e.latlng).addTo(map);
+    tmpMarker.setLatLng(e.latlng);
+    tmpMarker.bindPopup("<button class='set-point set-start' onclick='setStart()''>Set Start</button><button class='set-point set-end' onclick='setEnd()''>Set End</button>").openPopup();
 }
 
 function setStart() {
-	let coords = tmpMarker.getLatLng();
-	let lat = Math.round(coords.lat * 1000) / 1000;
-	let lng = Math.round(coords.lng * 1000) / 1000;
-	document.getElementById("start-text").innerHTML = "latitude: " + lat.toString() + "<br> longitude: " + lng.toString();
-	if (startMarker) {
-		map.removeLayer(startMarker);
-	}
-	startPoint = tmpMarker.getLatLng();
-	startMarker = L.marker(coords, {
-		icon: greenIcon
-	}).addTo(map);
-	map.removeLayer(tmpMarker);
-	if (typeof last_path === 'object') {
-		map.removeLayer(last_path);
-	}
+    let coords = tmpMarker.getLatLng();
+    let lat = Math.round(coords.lat * 1000) / 1000;
+    let lng = Math.round(coords.lng * 1000) / 1000;
+    document.getElementById("start-text").innerHTML = "latitude: " + lat.toString() + "<br> longitude: " + lng.toString();
+    if (startMarker) {
+        map.removeLayer(startMarker);
+    }
+    startPoint = tmpMarker.getLatLng();
+    startMarker = L.marker(coords, {
+        icon: greenIcon
+    }).addTo(map);
+    map.removeLayer(tmpMarker);
+    if (typeof last_path === 'object') {
+        map.removeLayer(last_path);
+    }
 }
 
 function setEnd() {
-	let coords = tmpMarker.getLatLng();
-	let lat = Math.round(coords.lat * 1000) / 1000;
-	let lng = Math.round(coords.lng * 1000) / 1000;
-	document.getElementById("end-text").innerHTML = "latitude: " + lat.toString() + "<br> longitude: " + lng.toString();
-	if (endMarker) {
-		map.removeLayer(endMarker);
-	}
-	endPoint = tmpMarker.getLatLng();
-	endMarker = L.marker(coords, {
-		icon: redIcon
-	}).addTo(map);
-	map.removeLayer(tmpMarker);
-	if (typeof last_path === 'object') {
-		map.removeLayer(last_path);
-	}
+    let coords = tmpMarker.getLatLng();
+    let lat = Math.round(coords.lat * 1000) / 1000;
+    let lng = Math.round(coords.lng * 1000) / 1000;
+    document.getElementById("end-text").innerHTML = "latitude: " + lat.toString() + "<br> longitude: " + lng.toString();
+    if (endMarker) {
+        map.removeLayer(endMarker);
+    }
+    endPoint = tmpMarker.getLatLng();
+    endMarker = L.marker(coords, {
+        icon: redIcon
+    }).addTo(map);
+    map.removeLayer(tmpMarker);
+    if (typeof last_path === 'object') {
+        map.removeLayer(last_path);
+    }
 }
 
 function query() {
-	hide_result();
-	hide_invalid_request();
-	hide_no_path_found();
-	hide_select_start_and_end();
+    hide_result();
+    hide_invalid_request();
+    hide_no_path_found();
+    hide_select_start_and_end();
 
-	if (typeof last_path === 'object') {
-		map.removeLayer(last_path);
-	}
-	
-	if (typeof startPoint === 'undefined' || typeof endPoint === 'undefined') {
-		show_select_start_and_end();
-		return;
-	}
+    if (typeof last_path === 'object') {
+        map.removeLayer(last_path);
+    }
 
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url + "dijkstra", true);
-	xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    if (typeof startPoint === 'undefined' || typeof endPoint === 'undefined') {
+        show_select_start_and_end();
+        return;
+    }
 
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			var json = JSON.parse(xhr.responseText);
-			if (json.path != "") {
-				printPath(json.path);
-				show_result(json.cost);
-			} else {
-				show_no_path_found();
-			}
-		} else if (xhr.readyState === 4) {
-			show_invalid_request();
-		}
-	};
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url + "dijkstra", true);
+    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
 
-	var travel_type = document.getElementById("travel-type").value;
-	var optimization = document.getElementById("optimization").value == "distance";
-	var body = {
-		"start": {
-			"latitude": startPoint.lat,
-			"longitude": startPoint.lng
-		},
-		"end": {
-			"latitude": endPoint.lat,
-			"longitude": endPoint.lng
-		},
-		"travel_type": travel_type,
-		"by_distance": optimization,
-	};
-	var data = JSON.stringify(body);
-	// console.log("request: " + data);
-	xhr.send(data);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            if (json.path != "") {
+                printPath(json.path);
+                show_result(json.cost);
+            } else {
+                show_no_path_found();
+            }
+        } else if (xhr.readyState === 4) {
+            show_invalid_request();
+        }
+    };
+
+    var travel_type = document.getElementById("travel-type").value;
+    var optimization = document.getElementById("optimization").value == "distance";
+    var body = {
+        "start": {
+            "latitude": startPoint.lat,
+            "longitude": startPoint.lng
+        },
+        "end": {
+            "latitude": endPoint.lat,
+            "longitude": endPoint.lng
+        },
+        "travel_type": travel_type,
+        "by_distance": optimization,
+    };
+    var data = JSON.stringify(body);
+    // console.log("request: " + data);
+    xhr.send(data);
 }
 
 
 function printPath(path) {
-	// create [lat, lng] array for leaflet map
-	let points = path.map(function(node) {
-		return [node.latitude, node.longitude]
+    // create [lat, lng] array for leaflet map
+    let points = path.map(function (node) {
+        return [node.latitude, node.longitude]
+    });
+    off_track_start = L.polyline([startPoint, points[0]], {
+		'dashArray': 10,
+		'weight': 2
+    });
+    start_to_end = L.polyline(points);
+    off_track_end = L.polyline([points[points.length - 1], endPoint], {
+		'dashArray': 10,
+		'weight': 2
 	});
-	last_path = L.polyline(points);
-	map.addLayer(last_path);
-	map.fitBounds(last_path.getBounds().pad(Math.sqrt(2) / 4));
+
+    last_path = L.layerGroup([off_track_start, start_to_end, off_track_end]);
+
+    map.addLayer(last_path);
+    map.fitBounds([startPoint, endPoint]);
 }
 
 
 function show_invalid_request() {
-	document.getElementById("invalid-request").style.display = "block";
+    document.getElementById("invalid-request").style.display = "block";
 }
+
 function hide_invalid_request() {
-	var x = document.getElementById("invalid-request");
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	}
+    var x = document.getElementById("invalid-request");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
 }
 
 function show_no_path_found() {
-	document.getElementById("no-path-found").style.display = "block";
+    document.getElementById("no-path-found").style.display = "block";
 }
+
 function hide_no_path_found() {
-	var x = document.getElementById("no-path-found");
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	}
+    var x = document.getElementById("no-path-found");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
 }
 
 function show_select_start_and_end() {
-	document.getElementById("select-start-and-end").style.display = "block";
+    document.getElementById("select-start-and-end").style.display = "block";
 }
+
 function hide_select_start_and_end() {
-	var x = document.getElementById("select-start-and-end");
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	}
+    var x = document.getElementById("select-start-and-end");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
 }
 
 function show_result(costs) {
-	var tmp = document.getElementById("result")
-	tmp.innerHTML = costs;
-	tmp.style.display = "block";
+    var tmp = document.getElementById("result")
+    tmp.innerHTML = costs;
+    tmp.style.display = "block";
 }
+
 function hide_result() {
-	var x = document.getElementById("result");
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	}
+    var x = document.getElementById("result");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
 }
 
 var greenIcon = new L.Icon({
-	iconUrl: 'img/marker-green.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+    iconUrl: 'img/marker-green.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
 var redIcon = new L.Icon({
-	iconUrl: 'img/marker-red.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+    iconUrl: 'img/marker-red.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
