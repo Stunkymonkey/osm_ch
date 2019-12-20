1cc66c543d952d5fd1bcabf51d93a2ee17ca3d51
Felix Buehler <account@buehler.rocks>
2019-12-21 00:58:31 +0100

    [pre] add min_heap struct

--- pre/src/min_heap.rs
+++ pre/src/min_heap.rs
@@ -1,20 +1,36 @@
-use std::collections::BinaryHeap;
+use constants::*;
+use std::cmp::Ordering;
 
-#[derive(Copy, Clone, Eq, PartialEq)]
-struct State {
-    node: usize,
-    cost: usize,
+#[derive(Copy, Clone, Eq)]
+pub struct MinHeapItem {
+    pub node: NodeId,
+    pub weight: Weight,
 }
 
 // Manually implement Ord so we get a min-heap instead of a max-heap
-impl Ord for State {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.cost.cmp(&self.cost)
+impl MinHeapItem {
+    pub fn new(node: NodeId, weight: Weight) -> MinHeapItem {
+        if weight != weight {
+            panic!("weight must not be NaN");
+        }
+        MinHeapItem { node, weight }
     }
 }
 
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+impl PartialOrd for MinHeapItem {
+    fn partial_cmp(&self, other: &MinHeapItem) -> Option<Ordering> {
         Some(self.cmp(other))
+    }
+}
+
+impl Ord for MinHeapItem {
+    fn cmp(&self, other: &MinHeapItem) -> Ordering {
+        self.weight.cmp(&other.weight).reverse()
+    }
+}
+
+impl PartialEq for MinHeapItem {
+    fn eq(&self, other: &MinHeapItem) -> bool {
+        self.weight == other.weight
     }
 }
