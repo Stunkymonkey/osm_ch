0ad00f3a4e5c317921168d036fb7fb0f0d3304df
Felix Buehler <account@buehler.rocks>
2020-01-14 11:57:49 +0100

    [pre] update constants

--- pre/src/constants.rs
+++ pre/src/constants.rs
@@ -1,15 +1,18 @@
 pub type NodeId = usize;
 pub type EdgeId = usize;
 pub type Weight = usize;
+pub type Rank = usize;
 pub type GridId = usize;
 
 pub const INVALID_NODE: NodeId = std::usize::MAX;
 pub const INVALID_EDGE: EdgeId = std::usize::MAX;
+pub const INVALID_RANK: Rank = std::usize::MAX;
 pub const WEIGHT_MAX: Weight = std::usize::MAX;
 pub const WEIGHT_ZERO: Weight = 0;
 
-// pub const COST_MULTIPLICATOR: usize = 262144; // 2^18
+pub const DIST_MULTIPLICATOR: usize = 262144; // 2^18
+pub const K_NEIGHBORS: usize = 2; // where the local minima should be located
 
-// ratio: north south 876km / west east 640 km = 100:136
+// ratio: north south 876km / west east 640 km ~ 100:136
 pub const LAT_GRID_AMOUNT: usize = 136;
 pub const LNG_GRID_AMOUNT: usize = 100;
