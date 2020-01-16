bc5b96c701a00d8f6970ccc295a09373c617234e
Felix Buehler <account@buehler.rocks>
2020-01-16 01:06:23 +0100

    [pre] add tests and unstable method

--- pre/src/offset.rs
+++ pre/src/offset.rs
@@ -11,7 +11,8 @@ fn fill_offset(edges: Vec<NodeId>, offset: &mut Vec<usize>) {
     }
 }
 
-pub fn generate_offsets(
+/// make sure edges are already sorted!!
+pub fn generate_offsets_unstable(
     edges: &mut Vec<Way>,
     mut up_offset: &mut Vec<EdgeId>,
     mut down_offset: &mut Vec<EdgeId>,
@@ -21,7 +22,6 @@ pub fn generate_offsets(
     down_offset.resize(amount_nodes + 1, 0);
 
     // generate up edges
-    edges.par_sort_unstable();
     let sources: Vec<EdgeId> = edges.par_iter().map(|x| x.source).rev().collect();
     fill_offset(sources, &mut up_offset);
 
@@ -45,79 +45,103 @@ pub fn generate_offsets(
     return down_index;
 }
 
+pub fn generate_offsets(
+    edges: &mut Vec<Way>,
+    mut up_offset: &mut Vec<EdgeId>,
+    mut down_offset: &mut Vec<EdgeId>,
+    amount_nodes: usize,
+) -> Vec<EdgeId> {
+    edges.par_sort_unstable();
+    return generate_offsets_unstable(edges, up_offset, down_offset, amount_nodes);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn fill_offset_test() {
-        let mut offset_test = vec![0; 8];
-        let mut edges = vec![0, 0, 0, 2, 3, 4, 4, 4, 6];
-        fill_offset(edges, &mut offset_test);
+        let mut offset = vec![0; 8];
+        let edges = vec![0, 0, 0, 2, 3, 4, 4, 4, 6];
+        fill_offset(edges, &mut offset);
 
-        println!("{:?}", offset_test);
-
-        assert_eq!(offset_test[0], 0);
-        assert_eq!(offset_test[1], 3);
-        assert_eq!(offset_test[2], 3);
-        assert_eq!(offset_test[3], 4);
-        assert_eq!(offset_test[4], 5);
-        assert_eq!(offset_test[5], 8);
-        assert_eq!(offset_test[6], 8);
-        assert_eq!(offset_test[7], 9);
+        assert_eq!(offset[0], 0);
+        assert_eq!(offset[1], 3);
+        assert_eq!(offset[2], 3);
+        assert_eq!(offset[3], 4);
+        assert_eq!(offset[4], 5);
+        assert_eq!(offset[5], 8);
+        assert_eq!(offset[6], 8);
+        assert_eq!(offset[7], 9);
     }
 
-    // TODO make good test
     #[test]
     fn all_offsets() {
-        let mut up_offset_test = vec![0; 8];
-        let mut down_offset_test = vec![0; 8];
+        //      7 -> 8 -> 9
+        //      |         |
+        // 0 -> 5 -> 6 -  |
+        // |         |  \ |
+        // 1 -> 2 -> 3 -> 4
+
+        let amount_nodes = 10;
+
         let mut edges = Vec::<Way>::new();
-        edges.push(Way {
-            source: 0,
-            target: 2,
-            weight: 0,
-            contrated_previous: None,
-            contrated_next: None,
-        });
-        edges.push(Way {
-            source: 1,
-            target: 2,
-            weight: 0,
-            contrated_previous: None,
-            contrated_next: None,
-        });
-        edges.push(Way {
-            source: 1,
-            target: 5,
-            weight: 0,
-            contrated_previous: None,
-            contrated_next: None,
-        });
-        edges.push(Way {
-            source: 4,
-            target: 5,
-            weight: 0,
-            contrated_previous: None,
-            contrated_next: None,
-        });
-        edges.push(Way {
-            source: 0,
-            target: 5,
-            weight: 0,
-            contrated_previous: None,
-            contrated_next: None,
-        });
-        let down_index_test =
-            generate_offsets(&mut edges, &mut up_offset_test, &mut down_offset_test, 8);
+        edges.push(Way::new(0, 1, 1));
+        edges.push(Way::new(1, 2, 1));
+        edges.push(Way::new(2, 3, 1));
+        edges.push(Way::new(3, 4, 20));
+        edges.push(Way::new(0, 5, 5));
+        edges.push(Way::new(5, 6, 1));
+        edges.push(Way::new(6, 4, 20));
+        edges.push(Way::new(6, 3, 20));
+        edges.push(Way::new(5, 7, 5));
+        edges.push(Way::new(7, 8, 1));
+        edges.push(Way::new(8, 9, 1));
+        edges.push(Way::new(9, 4, 1));
 
-        println!("{:?}", up_offset_test);
+        let mut up_offset = Vec::<EdgeId>::new();
+        let mut down_offset = Vec::<EdgeId>::new();
+        edges.par_sort_unstable();
+        let down_index =
+            offset::generate_offsets(&mut edges, &mut up_offset, &mut down_offset, amount_nodes);
 
-        for i in 0..edges.len() {
-            assert_eq!(
-                down_index_test[down_offset_test[edges[up_offset_test[i]].target]],
-                up_offset_test[i]
-            );
-        }
+        println!("{:?}", up_offset);
+
+        assert_eq!(up_offset[0], 0);
+        assert_eq!(up_offset[1], 2);
+        assert_eq!(up_offset[2], 3);
+        assert_eq!(up_offset[3], 4);
+        assert_eq!(up_offset[4], 5);
+        assert_eq!(up_offset[5], 5);
+        assert_eq!(up_offset[6], 7);
+        assert_eq!(up_offset[7], 9);
+        assert_eq!(up_offset[8], 10);
+        assert_eq!(up_offset[9], 11);
+        assert_eq!(up_offset[10], 12);
+
+        assert_eq!(down_offset[0], 0);
+        assert_eq!(down_offset[1], 0);
+        assert_eq!(down_offset[2], 1);
+        assert_eq!(down_offset[3], 2);
+        assert_eq!(down_offset[4], 4);
+        assert_eq!(down_offset[5], 7);
+        assert_eq!(down_offset[6], 8);
+        assert_eq!(down_offset[7], 9);
+        assert_eq!(down_offset[8], 10);
+        assert_eq!(down_offset[9], 11);
+        assert_eq!(down_offset[10], 12);
+
+        assert_eq!(down_index[0], 0);
+        assert_eq!(down_index[1], 2);
+        assert_eq!(down_index[2], 3);
+        assert_eq!(down_index[3], 7);
+        assert_eq!(down_index[4], 4);
+        assert_eq!(down_index[5], 8);
+        assert_eq!(down_index[6], 11);
+        assert_eq!(down_index[7], 1);
+        assert_eq!(down_index[8], 5);
+        assert_eq!(down_index[9], 6);
+        assert_eq!(down_index[10], 9);
+        assert_eq!(down_index[11], 10);
     }
 }
