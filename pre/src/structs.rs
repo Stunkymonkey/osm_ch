b3c5fa04bcc65f45ce8e0ce9ca6c927f25221720
Felix Buehler <account@buehler.rocks>
2020-01-16 01:07:24 +0100

    [pre] new structs for new architecture

--- pre/src/structs.rs
+++ pre/src/structs.rs
@@ -1,37 +1,87 @@
 use serde::Serialize;
+use std::cmp::Ordering;
 
 use constants::*;
 
-#[derive(Serialize, Debug, Clone)]
-pub struct Way {
-    pub source: usize,
-    pub target: usize,
+#[derive(Debug, PartialEq)]
+pub enum TravelType {
+    Car,
+    CarBicycle,
+    Bicycle,
+    BicyclePedestrian,
+    Pedestrian,
+    All,
+    Undefined,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum OptimizeBy {
+    Time,
+    Distance,
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct FullWay {
+    pub source: NodeId,
+    pub target: NodeId,
     pub speed: usize,
     pub distance: usize,
-    pub travel_type: usize,
+}
+
+#[derive(Serialize, Debug, Clone, Copy, Eq, PartialEq)]
+pub struct Way {
+    pub source: NodeId,
+    pub target: NodeId,
+    pub weight: usize,
+    // the result must be EdgeIds, but during execution this will be NodeIds
+    pub contrated_previous: Option<EdgeId>,
+    pub contrated_next: Option<EdgeId>,
+}
+
+impl PartialOrd for Way {
+    fn partial_cmp(&self, other: &Way) -> Option<Ordering> {
+        return Some(self.cmp(other));
+    }
+}
+
+impl Ord for Way {
+    fn cmp(&self, other: &Way) -> Ordering {
+        return self
+            .source
+            .cmp(&other.source)
+            .then(self.target.cmp(&other.target))
+            .then(self.contrated_previous.cmp(&other.contrated_previous))
+            .then(self.contrated_next.cmp(&other.contrated_next));
+    }
+}
+
+impl Way {
+    /// general constructor
+    pub fn new(from: NodeId, to: NodeId, weight: Weight) -> Self {
+        Way {
+            source: from,
+            target: to,
+            weight: weight,
+            contrated_previous: None,
+            contrated_next: None,
+        }
+    }
 }
 
 #[derive(Serialize, Debug, Clone)]
 pub struct Node {
     pub latitude: f32,
     pub longitude: f32,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-pub struct Shortcut {
-    pub from: NodeId,
-    pub to: NodeId,
-    pub center_node: NodeId,
-    pub weight: Weight,
+    pub rank: Rank,
 }
 
 #[derive(Serialize, Debug)]
 pub struct FmiFile {
     pub nodes: Vec<Node>,
-    pub edges: Vec<Way>,
     pub up_offset: Vec<EdgeId>,
-    pub down_index: Vec<EdgeId>,
     pub down_offset: Vec<EdgeId>,
-    pub grid_offset: Vec<usize>,
-    pub grid: Vec<usize>,
+    pub down_index: Vec<EdgeId>,
+    pub edges: Vec<Way>,
+    pub grid_offset: Vec<GridId>,
+    pub grid: Vec<NodeId>,
 }
