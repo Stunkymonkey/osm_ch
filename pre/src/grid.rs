a8091b40bf99dec9bd286eea4025759694146b14
Felix Buehler <account@buehler.rocks>
2020-01-26 23:55:55 +0100

    [pre] new grid structure from web

--- pre/src/grid.rs
+++ pre/src/grid.rs
@@ -22,18 +22,42 @@ fn get_min_max(nodes: &Vec<Node>) -> GridBounds {
         .map(|node| node.longitude)
         .max_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal))
         .unwrap();
-    return GridBounds{ lat_min, lat_max, lng_min, lng_max} ;
+    return GridBounds {
+        lat_min,
+        lat_max,
+        lng_min,
+        lng_max,
+    };
 }
 
-fn get_grid_id(node: &Node, grid_bounds: &GridBounds) -> usize {
-    let lat_percent = (node.latitude - grid_bounds.lat_min) / (grid_bounds.lat_max - grid_bounds.lat_min);
-    let lat_index = (lat_percent * (LAT_GRID_AMOUNT - 1) as f32) as usize;
-    let lng_percent = (node.longitude - grid_bounds.lng_min) / (grid_bounds.lng_max - grid_bounds.lng_min);
-    let lng_index = (lng_percent * (LNG_GRID_AMOUNT - 1) as f32) as usize;
-    return lng_index * LAT_GRID_AMOUNT + lat_index;
+fn get_grid_lat(node: &Node, grid_bounds: &GridBounds) -> usize {
+    let lat_percent =
+        (node.latitude - grid_bounds.lat_min) / (grid_bounds.lat_max - grid_bounds.lat_min);
+    return (lat_percent * (LAT_GRID_AMOUNT - 1) as f32) as usize;
 }
 
-pub fn generate_grid(grid: &mut Vec<GridId>, grid_offset: &mut Vec<usize>, nodes: &Vec<Node>) -> GridBounds{
+fn get_grid_lng(node: &Node, grid_bounds: &GridBounds) -> usize {
+    let lng_percent =
+        (node.longitude - grid_bounds.lng_min) / (grid_bounds.lng_max - grid_bounds.lng_min);
+    return (lng_percent * (LNG_GRID_AMOUNT - 1) as f32) as usize;
+}
+
+fn calculate_grid_id(lat_index: usize, lng_index: usize) -> GridId {
+    let grid_id = lng_index * LAT_GRID_AMOUNT + lat_index;
+    return grid_id;
+}
+
+fn get_grid_id(node: &Node, grid_bounds: &GridBounds) -> GridId {
+    let lat_index = get_grid_lat(node, grid_bounds);
+    let lng_index = get_grid_lng(node, grid_bounds);
+    return calculate_grid_id(lat_index, lng_index);
+}
+
+pub fn generate_grid(
+    grid: &mut Vec<GridId>,
+    grid_offset: &mut Vec<usize>,
+    nodes: &Vec<Node>,
+) -> GridBounds {
     grid.resize(nodes.len(), 0);
 
     let grid_bounds: GridBounds = get_min_max(nodes);
