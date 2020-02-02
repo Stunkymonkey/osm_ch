3a52ee1a97c42ab20296ed7fdba33eb02c531869
Felix Buehler <account@buehler.rocks>
2020-02-02 17:14:51 +0100

    [pre] remove redundant code in ordering

--- pre/src/ordering.rs
+++ pre/src/ordering.rs
@@ -1,42 +1,11 @@
 use super::*;
 
-/// amount of connected node
+/// amount of neighbors
 pub fn node_degree(node: NodeId, up_offset: &Vec<EdgeId>, down_offset: &Vec<EdgeId>) -> usize {
     return up_offset[node + 1] - up_offset[node] + down_offset[node + 1] - down_offset[node];
 }
 
-/// returning all previous and next neighbors
-pub fn get_neighbours(
-    node: NodeId,
-    edges: &Vec<Way>,
-    up_offset: &Vec<EdgeId>,
-    down_offset: &Vec<EdgeId>,
-    down_index: &Vec<EdgeId>,
-) -> (Vec<usize>, Vec<usize>) {
-    let prev = &up_offset[node..node + 1];
-    let next = &down_offset[node..node + 1];
-    let sources: Vec<NodeId> = prev.par_iter().map(|x| edges[*x].source).collect();
-    let targets: Vec<NodeId> = next
-        .par_iter()
-        .map(|x| edges[down_index[*x]].target)
-        .collect();
-    return (sources, targets);
-}
-
-/// returning all neighbors
-pub fn get_mixed_neighbours(
-    node: NodeId,
-    edges: &Vec<Way>,
-    up_offset: &Vec<EdgeId>,
-    down_offset: &Vec<EdgeId>,
-    down_index: &Vec<EdgeId>,
-) -> Vec<usize> {
-    let (sources, targets) = get_neighbours(node, edges, up_offset, down_offset, down_index);
-    let mut neighbours = sources;
-    neighbours.extend(&targets);
-    return neighbours;
-}
-
+/// calculating the edge-distance heuristic of single node
 fn edge_distance(
     node: NodeId,
     edges: &Vec<Way>,
@@ -44,8 +13,8 @@ fn edge_distance(
     down_offset: &Vec<EdgeId>,
     down_index: &Vec<EdgeId>,
     mut dijkstra: &mut dijkstra::Dijkstra,
-) -> usize {
-    let (shortcuts, _used_edges) = contraction::contract_node(
+) -> isize {
+    let (shortcuts, _used_edges) = contraction::calc_shortcuts(
         node,
         &edges,
         &up_offset,
