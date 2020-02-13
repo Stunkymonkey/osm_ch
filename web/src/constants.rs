4890c28678e4a89db9912fed1fe84b5ed00b8349
Felix Buehler <account@buehler.rocks>
2020-02-13 10:47:30 +0100

    [web/constants] remove unused variables

--- web/src/constants.rs
+++ web/src/constants.rs
@@ -5,10 +5,7 @@ pub type Rank = usize;
 pub type GridId = usize;
 
 pub const INVALID_NODE: NodeId = std::usize::MAX;
-pub const INVALID_EDGE: EdgeId = std::usize::MAX;
-pub const INVALID_RANK: Rank = std::usize::MAX;
 pub const WEIGHT_MAX: Weight = std::usize::MAX;
-pub const WEIGHT_ZERO: Weight = 0;
 
 pub const DIST_MULTIPLICATOR: usize = 262144; // 2^18
 
