fe35d29761d4ff7676366cb6e630d02205db973c
Felix Buehler <account@buehler.rocks>
2020-02-13 10:15:40 +0100

    [pre] allow dead-code in graph-helper

--- pre/src/graph_helper.rs
+++ pre/src/graph_helper.rs
@@ -1,16 +1,19 @@
 use super::*;
 
 /// get all up edges from one node
+#[allow(dead_code)]
 pub fn get_edges_from_id(ids: Vec<EdgeId>, edges: &Vec<Way>) -> Vec<Way> {
     return ids.par_iter().map(|x| edges[*x]).collect();
 }
 
 /// get all up edge-ids from one node
+#[allow(dead_code)]
 pub fn get_up_edge_ids(node: NodeId, up_offset: &Vec<EdgeId>) -> Vec<EdgeId> {
     return (up_offset[node]..up_offset[node + 1]).collect();
 }
 
 /// get all down edge-ids from one node
+#[allow(dead_code)]
 pub fn get_down_edge_ids(
     node: NodeId,
     down_offset: &Vec<EdgeId>,
@@ -21,6 +24,7 @@ pub fn get_down_edge_ids(
 }
 
 /// get all down edge-ids from one node
+#[allow(dead_code)]
 pub fn get_edge_ids(
     node: NodeId,
     up_offset: &Vec<EdgeId>,
@@ -33,6 +37,7 @@ pub fn get_edge_ids(
 }
 
 /// get all edge-ids from one node
+#[allow(dead_code)]
 pub fn get_all_edge_ids(
     node: NodeId,
     up_offset: &Vec<EdgeId>,
@@ -41,11 +46,13 @@ pub fn get_all_edge_ids(
 ) -> Vec<EdgeId> {
     let (outgoing, incomming) = get_edge_ids(node, &up_offset, &down_offset, &down_index);
     let mut connected_edges = outgoing;
+    // TODO merge in sorted manner
     connected_edges.extend(&incomming);
     return connected_edges;
 }
 
 /// get all up neighbors from one node
+#[allow(dead_code)]
 pub fn get_up_neighbors(node: NodeId, edges: &Vec<Way>, up_offset: &Vec<EdgeId>) -> Vec<EdgeId> {
     let next = get_up_edge_ids(node, &up_offset);
     let mut tmp: Vec<EdgeId> = next.par_iter().map(|x| edges[*x].target).collect();
@@ -54,6 +61,7 @@ pub fn get_up_neighbors(node: NodeId, edges: &Vec<Way>, up_offset: &Vec<EdgeId>)
 }
 
 /// get all up neighbors from one node
+#[allow(dead_code)]
 pub fn get_down_neighbors(
     node: NodeId,
     edges: &Vec<Way>,
@@ -68,6 +76,7 @@ pub fn get_down_neighbors(
 }
 
 /// returning all previous and next neighbors
+#[allow(dead_code)]
 pub fn get_neighbours(
     node: NodeId,
     edges: &Vec<Way>,
@@ -81,6 +90,7 @@ pub fn get_neighbours(
 }
 
 /// returning all neighbors
+#[allow(dead_code)]
 pub fn get_all_neighbours(
     node: NodeId,
     edges: &Vec<Way>,
@@ -89,6 +99,7 @@ pub fn get_all_neighbours(
     down_index: &Vec<EdgeId>,
 ) -> Vec<usize> {
     let (targets, sources) = get_neighbours(node, edges, up_offset, down_offset, down_index);
+    // TODO merge sorted vecs & dedup
     let mut neighbours = targets;
     neighbours.extend(&sources);
     return neighbours;
