f357cb50691dc601c06c7b505e31b5f6024dfe41
Felix Buehler <account@buehler.rocks>
2020-02-13 10:04:59 +0100

    [pre] remove unused constants

--- pre/src/constants.rs
+++ pre/src/constants.rs
@@ -5,13 +5,10 @@ pub type Rank = usize;
 pub type GridId = usize;
 
 pub const INVALID_NODE: NodeId = std::usize::MAX;
-pub const INVALID_EDGE: EdgeId = std::usize::MAX;
-pub const INVALID_RANK: Rank = std::usize::MAX;
 pub const WEIGHT_MAX: Weight = std::usize::MAX;
-pub const HEURISTIC_MAX: isize = std::isize::MAX;
+pub const INVALID_RANK: Rank = std::usize::MAX;
 
 pub const DIST_MULTIPLICATOR: usize = 262144; // 2^18
-pub const K_NEIGHBORS: usize = 2; // where the local minima should be located
 
 // ratio: north south 876km / west east 640 km ~ 100:136
 pub const LAT_GRID_AMOUNT: usize = 136;
