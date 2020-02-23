db9b4df1e13473e74f31d8bfcd481e7b5a6d95ee
Felix Buehler <account@buehler.rocks>
2020-02-23 18:41:43 +0100

    [pre] use RwLock instead of Mutex

--- pre/src/main.rs
+++ pre/src/main.rs
@@ -15,7 +15,7 @@ mod visited_list;
 use rayon::prelude::*;
 use std::collections::{BTreeSet, HashMap};
 use std::sync::atomic::{AtomicIsize, AtomicUsize, Ordering};
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 use std::time::Instant;
 
 use crate::constants::*;
